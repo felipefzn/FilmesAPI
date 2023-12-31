102956bc7885e6594c3179169aecbaeaf52009bc
Felipe Fazan <felipeffazandebrito@gmail.com>
2023-12-30 21:04:45 -0300

    Adicionando validacoes na classe Filme.cs

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -10,11 +10,12 @@ namespace FilmesAPI.Controllers
         private static List<Filme> filmes = new List<Filme>();
 
         [HttpPost]
-        public void AdicionaFilme([FromBody]Filme filme)
+        public void AdicionaFilme([FromBody] Filme filme)
         {
             filmes.Add(filme);
             Console.WriteLine("O Filme escolhido foi: " + filme.Titulo);
-            Console.WriteLine("Com uma duracao de: " + filme.Duracao);            
-        }   
+            Console.WriteLine("Com uma duracao de: " + filme.Duracao);
+            Console.WriteLine("Este filme é do genero : " + filme.Genero);
+        }
     }
 }
--- Models/Filme.cs
+++ Models/Filme.cs
@@ -1,9 +1,19 @@
-﻿namespace FilmesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
 {
     public class Filme
-    {        
-        public string Titulo { get; set; }        
+    {
+        [Required(ErrorMessage = "O Titulo do filme é obrigatório!")]
+        [MaxLength(100, ErrorMessage = "O tamanho máximo do titulo é de 100 caracteres!")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O genero do filme é obrigatório!")]
+        [MaxLength(50, ErrorMessage = "O tamanho do genero nao pode exceder 50 caracteres!")]
         public string Genero { get; set; }
+
+        [Required]
+        [Range(70, 400, ErrorMessage = "A duracao deve ser entre 70 e 400 minutos")]
         public int Duracao { get; set; }
     }
 }
